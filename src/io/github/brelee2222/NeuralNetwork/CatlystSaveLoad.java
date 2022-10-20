c56920bbf979d04f81c77c7389d6307afee0be73
BREgitLEEhub2222 <78449263+Brelee2222@users.noreply.github.com>
2022-10-20 11:08:20 -0700

    Version 1.1.0 Fully functioning save

--- src/io/github/brelee2222/NeuralNetwork/CatlystSaveLoad.java
+++ src/io/github/brelee2222/NeuralNetwork/CatlystSaveLoad.java
@@ -1,5 +1,6 @@
 package io.github.brelee2222.NeuralNetwork;
 
+import io.github.brelee2222.NeuralNetwork.Catlyst.CatlystNeuralNet;
 import io.github.brelee2222.NeuralNetwork.Network.NeuralNetwork;
 import io.github.brelee2222.NeuralNetwork.Neuron.Neuron;
 import io.github.brelee2222.NeuralNetwork.Neuron.NeuronInput;
@@ -8,6 +9,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
 
 public class CatlystSaveLoad {
     /**
@@ -15,20 +17,24 @@ public class CatlystSaveLoad {
      * 1. inputs (int)
      * 2. layers (int)
      * 3. layerSize (int) * layers
-     * 4. Neuron inputs (double)
+     * 4. Neuron inputs (double)...
      */
 
+    private static int index;
+    private static byte[] data;
+
     public static void save(NeuralNetwork network, String path) {
-        int size = 12 + network.neuronLayers.length*4;
+        int size = 8 + network.neuronLayers.length*4;
         for(int i = 0; i != network.neuronLayers.length; i++) for(Neuron neuron : network.neuronLayers[i])
             size += neuron.inputs.length*8;
-        byte[] data = new byte[size];
-        int index = 0;
-        index = writeInt(network.neuronLayers.length, data, writeInt(network.inputs, data, index));
+        data = new byte[size];
+        index = 0;
+        writeInt(network.inputs);
+        writeInt(network.neuronLayers.length);
         for(Neuron[] layer : network.neuronLayers)
-            index = writeInt(layer.length, data, index);
+            writeInt(layer.length);
         for(Neuron[] layer : network.neuronLayers) for(Neuron neuron : layer) for(NeuronInput input : neuron.inputs)
-            index = writeLong(Double.doubleToLongBits(input.getWeight()), data, index);
+            writeLong(Double.doubleToLongBits(input.getWeight()));
         try (FileOutputStream fileOutputStream = new FileOutputStream(path)) {
             fileOutputStream.write(data);
         } catch (FileNotFoundException e) {
@@ -38,17 +44,51 @@ public class CatlystSaveLoad {
         }
     }
 
-    public static int writeByte(byte value, byte[] data, int index) {
+    public static NeuralNetwork read(String path) {
+        try {
+            data = Files.readAllBytes(new File(path).toPath());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        index = 0;
+        int inputs = readInt();
+        int layers = readInt();
+
+        int[] layerSizes = new int[layers];
+        for(int i = 0; i != layers; i++)
+            layerSizes[i] = readInt();
+        NeuralNetwork network = CatlystNeuralNet.makeNetwork(inputs, layerSizes, 0, 0);
+        for(int i = 0; i != layers; i++) for(Neuron neuron : network.neuronLayers[i]) for(NeuronInput neuronInput : neuron.inputs)
+            neuronInput.setWeight(Double.longBitsToDouble(readLong()));
+        return network;
+    }
+
+    public static byte readByte() {
+        return data[index++];
+    }
+    public static short readShort() {
+        return (short) ((short) readByte() << 8 | readByte() & 0xff);
+    }
+    public static int readInt() {
+        return (int) readShort() << 16 | readShort() & 0xffff;
+    }
+    public static long readLong() {
+        return (long) readInt() << 32 | readInt() & 0xffffffffL;
+    }
+
+    public static void writeByte(byte value) {
         data[index++] = value;
-        return index;
+    } // could've made a universal writeFUnction
+    public static void writeShort(short value) {
+        writeByte((byte) (value >> 8));
+        writeByte((byte) value);
     }
-    public static int writeShort(short value, byte[] data, int index) {
-        return writeByte((byte) (value & 0xff), data, writeByte((byte) ((value >> 8) & 0xff), data, index));
+    public static void writeInt(int value) {
+        writeShort((short) (value >> 16));
+        writeShort((short) value);
     }
-    public static int writeInt(int value, byte[] data, int index) {
-        return writeShort((short) (value & 0xffff), data, writeShort((short) ((value >> 16) & 0xffff), data, index));
-    }
-    public static int writeLong(long value, byte[] data, int index) {
-        return writeInt((int) value, data, writeInt((int) (value >> 32), data, index));
+    public static void writeLong(long value) {
+        writeInt((int) (value >> 32));
+        writeInt((int) value);
     }
 }
