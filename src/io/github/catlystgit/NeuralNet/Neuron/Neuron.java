631188c7b619d4b5c02ac1a5e015d59827a70a53
BREgitLEEhub2222 <78449263+Brelee2222@users.noreply.github.com>
2022-11-07 09:55:25 -0800

    chagnes

--- src/io/github/catlystgit/NeuralNet/Neuron/Neuron.java
+++ src/io/github/catlystgit/NeuralNet/Neuron/Neuron.java
@@ -6,10 +6,6 @@ public interface Neuron {
     double compute();
     double get(); // returns the input * weight and activation
     double getErrSignal(); // returns the errSignal of the neuron
-    void setResult(double result); // sets the result value for get (used for input feedback)
     void setErrSignal(double signal); // gives an error signal. Should give a different result from getErrSignal()
     void addErrSignal(double signal); // adds err signal
-    void setTargetResult(double result); // Inputfeeback
-    double getTargetResult(); // For input feedback
-    NeuronInput getBias();
 }
